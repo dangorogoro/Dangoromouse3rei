9a0b3d244b15efbe4e84825b6520f705f2380493
dangorogoro <dangorogoro0802@gmail.com>
2019-10-14 19:07:48 +0900

    Fix include problem

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -7,7 +7,22 @@
 
 #ifndef INC_CONFIG_H_
 #define INC_CONFIG_H_
-#include "main.h"
+
+#include "stm32f4xx_hal.h"
+#define ARM_MATH_CM4
+#include "SEGGER_RTT.h"
+#include "arm_math.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "task.h"
+#include "semphr.h"
+#include "queue.h"
+#include "list.h"
+
 typedef struct{
 	bool gyro;
 	bool ir_reset;
@@ -19,7 +34,7 @@ typedef struct{
 extern Flag robotFlag;
 extern uint16_t left_IR_value, right_IR_value;
 extern uint16_t left_IR_tmp_value, right_IR_tmp_value;
-extern uint32_t g_ADCBuffer[];
+extern uint16_t g_ADCBuffer[];
 
 extern uint8_t mode_select();
 extern void debug_task();
