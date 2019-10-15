b45f4857d45be707a57792069bec483081c487d8
dangorogoro <dangorogoro0802@gmail.com>
2019-10-16 06:19:35 +0900

    Change Tick frequency

--- FreeRTOS/Inc/FreeRTOSConfig.h
+++ FreeRTOS/Inc/FreeRTOSConfig.h
@@ -81,7 +81,7 @@
 #define configUSE_IDLE_HOOK    1
 #define configUSE_TICK_HOOK    0//vApplicationStackOverflowHook
 #define configCPU_CLOCK_HZ    ( 100 * 1000 * 1000 )
-#define configTICK_RATE_HZ    ( ( TickType_t ) 1000 )
+#define configTICK_RATE_HZ    ( ( TickType_t ) 100000 )
 #define configMAX_PRIORITIES   ( 5 )
 #define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 130 )
 #define configTOTAL_HEAP_SIZE   ( ( size_t ) ( 75 * 1024 ) )
@@ -93,7 +93,7 @@
 #define configQUEUE_REGISTRY_SIZE  8
 #define configCHECK_FOR_STACK_OVERFLOW 0
 #define configUSE_RECURSIVE_MUTEXES  1
-#define configUSE_MALLOC_FAILED_HOOK 1
+#define configUSE_MALLOC_FAILED_HOOK 0
 #define configUSE_APPLICATION_TASK_TAG 0
 #define configUSE_COUNTING_SEMAPHORES 1
 #define configGENERATE_RUN_TIME_STATS 0
