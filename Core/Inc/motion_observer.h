3bdaf5811b4f0c21f4d1394b11d9b708a5b213a1
dangorogoro <dangorogoro0802@gmail.com>
2019-10-22 04:59:24 +0900

    Add a clss for robot state

--- Core/Inc/motion_observer.h
+++ Core/Inc/motion_observer.h
@@ -10,12 +10,41 @@
 #include "spi.h"
 #include "main.h"
 #include "encoder.h"
-#include "TaskBase.h"
+struct MotionInput{
+	float v,w;
+	MotionInput(float _v = 0, float _w = 0) : v(_v), w(_w){}
+};
+
+struct RobotState{
+	float x, y, theta;
+	RobotState(float _x = 0.0f, float _y = 0.0f, float _theta = PI / 2.0) : x(_x), y(_y), theta(_theta){}
+  inline void operator+=(const RobotState &obj) { x+=obj.x; y+=obj.y; theta+=obj.theta;}
+  inline void operator-=(const RobotState &obj) { x-=obj.x; y-=obj.y; theta-=obj.theta;}
+  void update(const MotionInput& state_input, float time_step = 0.001f){
+		float sin_theta, cos_theta;
+		float sin_delta_theta, cos_delta_theta;
+		arm_sin_cos_f32(theta / PI * 180.0, &sin_theta, &cos_theta);
+		arm_sin_cos_f32((theta + state_input.w * time_step) / PI * 180.0, &sin_delta_theta, &cos_delta_theta);
+  	if(abs(state_input.w) > 0.001){
+  		x += -state_input.v / state_input.w * sin_theta + state_input.v / state_input.w * sin_delta_theta;
+  		y +=  state_input.v / state_input.w * cos_theta - state_input.v / state_input.w * cos_delta_theta;
+  	}
+  	else{
+  		x += state_input.v * cos_theta;
+  		y += state_input.v * sin_theta;
+  	}
+  	theta += state_input.w * time_step;
+  }
+
+};
 class MotionObserver{
 private:
 	Encoder encoder;
 	ICM20602 imu;
 	TaskHandle_t handle;
+	EncoderUnit present_velocity;
+	GyroData present_gyro;
+	MotorControl motor_control;
 public:
 
 	//MotionObserver() :TaskBase((const char*)"Montion", 3, 1024){}
--- Core/Src/motion_observer.cpp
+++ Core/Src/motion_observer.cpp
@@ -4,9 +4,8 @@
  *  Created on: Oct 13, 2019
  *      Author: dango
  */
-
 #include "motion_observer.h"
-
+float target_velocity = 300.0;
 void MotionObserver::createTask(const char*name, const uint16_t& stack_size,
 		const UBaseType_t& task_priority){
 	xTaskCreate([](void* obj){
@@ -16,21 +15,59 @@ void MotionObserver::createTask(const char*name, const uint16_t& stack_size,
 
 void MotionObserver::init(){
 	imu.setCalibration();
+	motor_control.pidInit();
 }
 void MotionObserver::task(){
 	portTickType xLastWakeTime;
-	const portTickType xFrequency = 10 * 1000;
+	const uint32_t calledFrequency = 1000;
+	const portTickType xFrequency = configTICK_RATE_HZ / calledFrequency;
 	xLastWakeTime = xTaskGetTickCount();
+	uint16_t count = 0;
+	bool flag = false;
+	RobotState robot_state;
+	MotionInput state_input;
 	while(1){
-
+		count = (count + 1) % 10000;
 		auto observed_gyro = imu.update();
-		printf("gyro %f %f\n",observed_gyro.x_accel, observed_gyro.z_gyro);
 		auto observed_encoder = encoder.update();
-		printf("TASK LEFT %f RIGHT %f\n", observed_encoder.left_encoder_velocity,
-				observed_encoder.right_encoder_velocity);
+		float v = (observed_encoder.left_encoder_velocity +
+				observed_encoder.right_encoder_velocity) / 2.0 * calledFrequency;
+		float w = observed_gyro.z_gyro;
 
-		//printf("ADC %d, %d, %d\n", g_ADCBuffer[0], g_ADCBuffer[1], g_ADCBuffer[2]);
+		/*  uint32_t data;
+		 *
+		if(count >= 1000 && flag == false){
+			if(count >= 1300){
+				stop_motor();
+				flag = true;
+			}
+			else	set_motor_pulse(100, 100);
+			//printf("gyro %f %f\n",observed_gyro.x_accel, observed_gyro.z_gyro);
+			if(count % 10 == 0){
+				printf("%f, %f, %f, %f %f\n",
+						o  uint32_t data;
+						bserved_encoder.left_encoder_velocity * calledFrequency,
+						observed_encoder.right_encoder_velocity * calledFrequency,
+						observed_gyro.x_accel,
+						observed_gyro.y_accel,
+						observed_gyro.z_gyro);
+			}
+		}
+		*/
+		state_input = MotionInput(v, w);
+		robot_state.update(state_input);
+
+		//motor_control.pidControlUpdate(target_velocity - v, 0 - w);
+		if(count % 100 == 0){
+			MotionInput state_input;
+	printf("motion -> %f, %f \n", state_input.v, state_input.w);
+			printf("robot_state %f, %f, %f\n", robot_state.x, robot_state.y, robot_state.theta);
+		}
+		if(robot_state.y > 90)
+	    HAL_GPIO_WritePin(GPIOH, GPIO_PIN_0, GPIO_PIN_RESET);
+		else
+			HAL_GPIO_WritePin(GPIOH, GPIO_PIN_0, GPIO_PIN_SET);
+
 		vTaskDelayUntil(&xLastWakeTime, xFrequency);
-		//vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
 }
