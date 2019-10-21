7bdd41e03cc1a1ac52ec6e4a011de4dde5cfd62a
dangorogoro <dangorogoro0802@gmail.com>
2019-10-22 04:46:53 +0900

    Change frequency for RTOS

--- FreeRTOS/Inc/FreeRTOSConfig.h
+++ FreeRTOS/Inc/FreeRTOSConfig.h
@@ -81,7 +81,7 @@
 #define configUSE_IDLE_HOOK    1
 #define configUSE_TICK_HOOK    0//vApplicationStackOverflowHook
 #define configCPU_CLOCK_HZ    ( 100 * 1000 * 1000 )
-#define configTICK_RATE_HZ    ( ( TickType_t ) 100000 )
+#define configTICK_RATE_HZ    ( ( TickType_t ) 100 * 1000 )
 #define configMAX_PRIORITIES   ( 5 )
 #define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE   ( ( size_t ) ( 75 * 1024 ) )
