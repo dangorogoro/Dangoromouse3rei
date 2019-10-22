f3e0196a9730dff4c8d150d4f71fcfb18f08b6f0
dangorogoro <dangorogoro0802@gmail.com>
2019-10-22 18:11:39 +0900

    Add PID controller for motor

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -13,13 +13,14 @@ void set_right_motor_pulse(int16_t right_pulse);
 void set_motor_pulse(int16_t left_pulse, int16_t right_pulse);
 void start_motor();
 void stop_motor();
+void estimate_velocity();
 class MotorControl{
 private:
 public:
 	MotorControl(){}
 	void pidInit();
-	void pidControlUpdate(const float& left_diff,
-			const float& right_diff);
+	void pidControlUpdate(const float& v_diff,
+			const float& w_diff);
 	void pidReset();
 };
 #endif /* INC_MOTOR_H_ */
