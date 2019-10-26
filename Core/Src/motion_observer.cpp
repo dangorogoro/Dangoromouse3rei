101976fc936b528fd05dfedc547425d9d120a77b
dangorogoro <dangorogoro0802@gmail.com>
2019-10-26 12:59:49 +0900

    Control motor through message queue

--- Core/Src/motion_observer.cpp
+++ Core/Src/motion_observer.cpp
@@ -8,13 +8,13 @@
 float target_velocity = 300.0;
 float target_w = 5;
 
-float search_velocity_accel = 5.0;
 
-float rad_accel = 0.4;
-
-float alpha = 0.65;
+constexpr float alpha = 0.65;
 float last_velocity = 0.0, present_velocity = 0.0;
 float last_sum_accel = 0.0, sum_accel = 0.0;
+
+xQueueHandle FixedRobotStateQueue = xQueueCreate(1, sizeof(RobotState));
+
 float get_fusioned_velocity(const float& raw_velocity, const float& raw_accel){
 	last_sum_accel = sum_accel;
 	last_velocity = present_velocity;
@@ -43,6 +43,7 @@ void MotionObserver::task(){
 	bool flag = false;
 	RobotState robot_state;
 	MotionInput state_input;
+	uint16_t count = 0;
 	float recent_target_velocity = 0.0;
 	float recent_target_w = 0.0;
 	while(1){
@@ -58,13 +59,20 @@ void MotionObserver::task(){
 		state_input = MotionInput(v, w);
 		robot_state.update(state_input);
 
-		/*
-		/ount = (count + 1) % 10000;
+
+		MotionInput receive_input;
+		count = (count + 1) % 10000;
+		xQueueReceive(TargetInputQueue, &receive_input, (TickType_t) 0);
+		xQueueSendToBack(RobotStateQueue, &robot_state, (TickType_t) 0);
 		if(count % 100 == 0){
+			printf("receive input %f, %f\n", receive_input.v, receive_input.w);
+			/*
 			printf("motion -> %f, %f \n", state_input.v, state_input.w);
 			printf("robot_state %f, %f, %f\n", robot_state.x, robot_state.y, robot_state.theta);
+			*/
 		}
-		*/
+		xQueueReceive(FixedRobotStateQueue, &robot_state, 0);
+		/*
 		//if(target_velocity > recent_target_velocity)	recent_target_velocity += 5.0;
 		if(target_w > recent_target_w) recent_target_w += rad_accel;
 		if(robot_state.theta > 2 * PI){
@@ -72,8 +80,10 @@ void MotionObserver::task(){
 			stop_motor();
 		}
 		else if(flag == false){
-			motor_control.pidControlUpdate(recent_target_velocity - v, recent_target_w - w);
+			//motor_control.pidControlUpdate(recent_target_velocity - v, recent_target_w - w);
 		}
+		*/
+		motor_control.pidControlUpdate(receive_input.v - v, receive_input.w - w);
 
 		vTaskDelayUntil(&xLastWakeTime, xFrequency);
 	}
