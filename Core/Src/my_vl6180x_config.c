b24025f34233914bceac8fbb916c1c5efdd4440a
dangorogoro <dangorogoro0802@gmail.com>
2019-10-26 12:26:16 +0900

    Fix configuration for ToF

--- Core/Src/my_vl6180x_config.c
+++ Core/Src/my_vl6180x_config.c
@@ -1,13 +1,13 @@
 /*
- * vl6180x_config.c
+ * my_vl6180x_config.c
  *
  *  Created on: Oct 19, 2019
  *      Author: dango
  */
+#include "my_vl6180x_config.h"
 #include "vl6180x_api.h"
 #include "config.h"
-#define i2c_bus      (&hi2c3)
-#define def_i2c_time_out 10
+
 int VL6180x_I2CWrite(VL6180xDev_t addr, uint8_t *buff, uint8_t len) {
   int status;
   status = HAL_I2C_Master_Transmit(i2c_bus, addr, buff, len, def_i2c_time_out);
