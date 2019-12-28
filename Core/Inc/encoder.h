db038ea7b6a0cc91fdd1d4d38acbc4b0158dd797
dangorogoro <dangorogoro0802@gmail.com>
2019-12-28 16:21:55 +0900

    Fix size of wheel

--- Core/Inc/encoder.h
+++ Core/Inc/encoder.h
@@ -9,7 +9,7 @@
 #define INC_ENCODER_H_
 
 #include "config.h"
-constexpr float CIRCUMFERENCE = 13.2 * PI;
+constexpr float CIRCUMFERENCE = 13.0 * PI;
 constexpr float ENCODER_RESOLUTION = 1024.0;
 constexpr float ROBOT_WIDTH = 32.0f;
 struct EncoderUnit{
